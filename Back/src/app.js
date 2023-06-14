680b588fed1d5f41d47cc47e39291e93120ecf7d
LautaRivata <Lautarivata@gmail.com>
2023-06-14 11:24:15 -0300

    Restructuracion de Carpetas en el Back

--- Back/src/app.js
+++ Back/src/app.js
@@ -14,7 +14,7 @@ const registerRouter = require('./routes/register');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.resolve(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
