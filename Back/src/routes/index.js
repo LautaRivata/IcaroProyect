f09fabeb4e47b7398bd12df29a8d176dc4e42be7
LautaRivata <Lautarivata@gmail.com>
2023-06-21 23:18:30 -0300

    Falta los Css

--- Back/src/routes/index.js
+++ Back/src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Direccion Index, Metodo: GET');
+  res.render('home');
 });
 
 module.exports = router;
