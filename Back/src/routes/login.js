ca50d051c9fdb8b620297b584b9b102287edec9d
LautaRivata <Lautarivata@gmail.com>
2023-06-27 23:23:49 -0300

    Incorporacion de Pretier para el codigo, inicializaciones para el loggin

--- Back/src/routes/login.js
+++ Back/src/routes/login.js
@@ -1,22 +1,30 @@
-const express = require('express');
-const bcrypt = require("bcrypt"); 
+const express = require("express");
+const bcrypt = require("bcrypt");
 
-const { getLoginController, postLoginController, getMyProfile, getLogin, getLogout } = require("../controllers/login")
+const {
+  getLoginController,
+  postLoginController,
+  getMyProfile,
+  getLogin,
+  getLogout,
+} = require("../controllers/login");
 
 const router = express.Router();
 
 /****************************** Routes  ********************************/
 
-router.get('/', getLoginController);
+router.get("/", getLoginController);
 
-router.post('/', postLoginController)
+router.post("/", postLoginController);
 
-router.get("/myProfile", getMyProfile)
+router.get("/myProfile", getMyProfile);
 
-router.get("/login", (req, res, next) => res.render("login", { title: 'Login' }))
+router.get("/login", (req, res, next) =>
+  res.render("login", { title: "Login" })
+);
 
-router.post("/login", getLogin)
+router.post("/login", getLogin);
 
-router.get('/logout', getLogout)
+router.get("/logout", getLogout);
 
 module.exports = router;
--- Back/src/services/users.js
+++ Back/src/services/users.js
@@ -1,52 +1,50 @@
 const fs = require("fs");
 const path = require("path");
 
-
-function readDB(){
-  try{
-    const filePath = path.join(__dirname, "..", '..', "db/usersDB.json"); // Obtengo la ruta completa
+function readDB() {
+  try {
+    const filePath = path.join(__dirname, "..", "..", "db/usersDB.json"); // Obtengo la ruta completa
     const data = fs.readFileSync(filePath, "utf8");
     const dataParseada = JSON.parse(data, null, 2);
-    return dataParseada
+    return dataParseada;
   } catch (error) {
-    console.error(error)
-    return false 
+    console.error(error);
+    return false;
   }
-  
 }
 
 function createUsers(user) {
   try {
-    const filePath = path.join(__dirname, "..", '..',"db/usersDB.json"); // Obtengo la ruta completa
+    const filePath = path.join(__dirname, "..", "..", "db/usersDB.json"); // Obtengo la ruta completa
     // const data = getAllProducts();
-    const data = []
+    const data = [];
     data.push(user);
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-    return true
+    return true;
   } catch (error) {
-    console.error(error)
-    return false
+    console.error(error);
+    return false;
   }
 }
 
 function findUserByUsername(email) {
-    dataParseada = readDB()
-    if (dataParseada.length) {
-      const savedUser = dataParseada.filter((user) => user.email === email)
+  dataParseada = readDB();
+  if (dataParseada.length) {
+    const savedUser = dataParseada.filter((user) => user.email === email);
 
-      return savedUser.length ? savedUser[0] : false
-    }
-    return false
+    return savedUser.length ? savedUser[0] : false;
+  }
+  return false;
 }
 
-function findIdDisponible(){
-  dataParseada = readDB()
-  for(let i = 0; i < dataParseada.lenght; i++){
-    if (dataParseada[i].id == NaN){
-      return i
+function findIdDisponible() {
+  dataParseada = readDB();
+  for (let i = 0; i < dataParseada.lenght; i++) {
+    if (dataParseada[i].id == NaN) {
+      return i;
     }
   }
-  return(dataParseada.length + 1)
+  return dataParseada.length + 1;
 }
 
-module.exports = { createUsers, findUserByUsername, findIdDisponible }
+module.exports = { createUsers, findUserByUsername, findIdDisponible };
